01b857cb29982f5fa1d866305e7c7e99f14267cb
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2018-01-06 22:14:23 -0800

    update2
    another if else block etc.

--- TempConversion.php
+++ TempConversion.php
@@ -2,9 +2,11 @@
 
 $radioVal = $_POST["Temp"];
 
+if (is_numeric($_POST['Num'])) {
+
 if(isset($_POST['Num']) and $radioVal == "FtoC")
 {
- echo $_POST['Num'] . ' Fahrenheit is ' .  round(($_POST['Num'] - 32)/1.8) . ' Celsius. <br><hr><br>';
+ echo '<h2>' . $_POST['Num'] . ' Fahrenheit is ' .  round(($_POST['Num'] - 32)/1.8) . ' Celsius. </h2><br><hr><br>';
 echo '
 <form action="" method="post">
 <input type="radio" name="Temp" value="FtoC" checked>Fahrenheit to Celsius<br> 
@@ -34,7 +36,7 @@ Enter Number: <br>
 
 elseif(isset($_POST['Num']) and $radioVal == "CtoF")
 {
- echo $_POST['Num'] . ' Celsius is ' .  round(($_POST['Num'] * 1.8 + 32)) . ' Fahrenheit. <br><hr><br>';
+ echo '<h2>' . $_POST['Num'] . ' Celsius is ' .  round(($_POST['Num'] * 1.8 + 32)) . ' Fahrenheit. </h2><br><hr><br>';
 echo '
 <form action="" method="post">
 <input type="radio" name="Temp" value="FtoC" checked>Fahrenheit to Celsius<br> 
@@ -64,7 +66,7 @@ Your Number: <br>
 
 elseif(isset($_POST['Num']) and $radioVal == "FtoK")
 {
- echo $_POST['Num'] . ' Fahrenheit is ' .  round(($_POST['Num'] + 459.67) * 5/9) . ' Kelvin. <br><hr><br>';
+ echo '<h2>' . $_POST['Num'] . ' Fahrenheit is ' .  round(($_POST['Num'] + 459.67) * 5/9) . ' Kelvin. </h2><br><hr><br>';
 echo '
 <form action="" method="post">
 <input type="radio" name="Temp" value="FtoC" checked>Fahrenheit to Celsius<br> 
@@ -118,5 +120,36 @@ Your Number: <br>
 
 </form>';
 
+} 
+
+}else{
+    
+echo ' <h2>Please enter numbers only.</h2>
+<hr>
+<form action="" method="post">
+<input type="radio" name="Temp" value="FtoC" checked>Fahrenheit to Celsius<br> 
+<input type="radio" name="Temp" value="CtoF">Celsius to Fahrenheit<br>
+<input type="radio" name="Temp" value="FtoK">Fahrenheit to Kelvin<br>
+<label>
+Your Number: <br>
+ <input
+  type="text"
+  name="Num"
+  placeholder="Enter a number"
+  required="required"
+  size="30"
+  maxlength="60"
+  autofocus
+  title="Number is is Required"
+  tabindex="10"
+  />
+</label>
+
+
+ <br>
+<input type="submit" />
+
+</form>';
+    
 }
  ?>
